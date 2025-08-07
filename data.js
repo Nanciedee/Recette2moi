50058c491e73357f8e5e5717bc42b0ca1360011e
Nanciedee <nanciedee001@gmail.com>
2025-08-06 21:19:34 -0400

    Create data.js
    "créations cartes de recettes"

---
+++ data.js
@@ -0,0 +1,40 @@
+const recipes = [
+    {
+        id: 'lundi-lunch',
+        day: 'Lundi',
+        meal: 'Lunch',
+        title: 'Salade de Poulet et Céleri sur Pain Tranché',
+        prepTime: '10 minutes',
+        cookTime: '0 minutes',
+        portions: '3',
+        ingredients: [
+            '2 cannes de 156g de flocons de poulet',
+            'Céleri coupé en morceaux (environ 1/4 tasse)',
+            'Mayonnaise (environ 50g)',
+            'Pain tranché blanc (6 tranches)',
+            'Sel et poivre au goût'
+        ],
+        instructions: [
+            "Égouttez la canne de flocons de poulet soigneusement.",
+            "Dans un bol moyen, émiettez le poulet à l'aide d'une fourchette.",
+            "Coupez le céleri en très petits dés et ajoutez-le au poulet.",
+            // ... Ajoutez le reste des étapes
+        ],
+        nutrition: {
+            calories: '~300 kcal',
+            proteins: '~15g',
+            carbs: '~25g',
+            fats: '~15g',
+            fibers: '~2g',
+            sodium: '~400 mg'
+        }
+    },
+    {
+        id: 'lundi-dinner',
+        day: 'Lundi',
+        meal: 'Dîner',
+        title: 'Pâtes Penne Rigate à la Bolognaise',
+        // ... Ajoutez le reste des détails pour cette recette
+    },
+    // ... Ajoutez toutes les autres recettes de la semaine ici
+];
